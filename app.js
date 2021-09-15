ffc2a9db3fcf15c6484f29736079b1fb2b9d3845
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-16 01:04:40 +0600

    Fixed Technical Error Issue in search field

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const inputBtn = document.getElementById('input-btn');
 const inputField = document.getElementById('input-field');
+const countryName = document.getElementById('country-name');
+
+
+// Search Countries University:
+inputBtn.addEventListener('click', function () {
+        const inputValue = inputField.value;
+        document.getElementById('item-container').textContent = '';
+        // If User Search Nothing:
+        if (inputValue === '') {
+                countryName.innerHTML = `
+                                <span class="text-danger"> Please Write Country Name </span>           
+                        `;
+                return;
+        }
+        fetch(`http://universities.hipolabs.com/search?country=${inputValue}`)
+                .then(res => res.json())
+                .then(data => {
+                        displayData(data)
+                        if (data.length >= 1) {
+                                countryName.innerHTML = `
+                                                Universities of <span
+                                                class="text-warning">${inputValue} </span>
+                                        `;
+                        }
+                        else {
+                                countryName.innerHTML = `
+                                        Result not found <span
+                                        class="text-warning">${inputValue} </span>
+                                `;
+                        }
+                })
+
+        inputField.value = '';
+})
+
+// Show All University Result After Search:
+// const displaySearchResult = (universities)
 
 const loadData = () => {
-        fetch(`http://universities.hipolabs.com/search?country=United+States`)
+        fetch(`http://universities.hipolabs.com/search?country=bangladesh`)
                 .then(res => res.json())
                 .then(data => displayData(data))
 }
@@ -16,13 +53,12 @@ const displayData = (universities) => {
         universities.forEach((university) => {
                 // console.log(university);
                 const website = university.web_pages;
-                console.log(website);
                 const div = document.createElement('div');
                 div.innerHTML = `
                         <div class="col">
                                 <div class="h-100 item-card">
                                         <div class="card-body">
-                                                <div style="height: 90px;">
+                                                <div style="height: 120px;">
                                                         <h4 class="card-title overflow-hidden text-center text-success"><i class="fas fa-university text-danger"></i> ${university.name} 
                                                         </h4>
                                                 </div>
